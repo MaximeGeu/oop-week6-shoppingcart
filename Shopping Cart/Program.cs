0964684c812f1424e6b145f1c55c98ebad2638db
Maxime Geniesse <maxime.geniesse@edu.devinci.fr>
2020-10-19 18:56:04 +0100

    Ajoutez des fichiers projet.

---
+++ Shopping Cart/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping_Cart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product apple = new Product { Name = "Apple", Price = 0.6 };
+            Product orange = new Product { Name = "Orange", Price = 0.25 };
+
+            List<Product> list = new List<Product>();
+            list.Add(apple);
+            list.Add(apple);
+            list.Add(orange);
+            list.Add(apple);
+            list.Add(orange);
+
+            Till till = new Till();
+            till.Sum(list);
+
+            Console.WriteLine($"Total price is {till.Total}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Shopping Cart/Till.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping_Cart
+{
+    class Till
+    {
+        public double Total { get; set; }
+
+        public double Sum(List<Product> list)
+        {
+            double total = 0;
+
+            for (int i = 0; i < list.Count(); i++)
+            {
+                total = total + list[i].Price;
+            }
+            Total = total;
+
+            return total;
+        }
+    }
+}
